27500e443d7b683d9b146d1f859584b82d3d9ade
adubz510 <adubz510@gmail.com>
2024-12-16 19:40:09 -0700

    turned S to s back in psql-setup-script.js

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,7 +1,7 @@
-const { Sequelize } = require('./db/models');
+const { sequelize } = require('./db/models');
 
-Sequelize.showAllSchemas({ logging: false }).then(async (data) => {
+sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
-    await Sequelize.createSchema(process.env.SCHEMA);
+    await sequelize.createSchema(process.env.SCHEMA);
   }
 });
